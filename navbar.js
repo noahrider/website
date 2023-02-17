047a356cdb22fb72ba11bec0ac5ddb0285929b30
Noah Rider <97214639+noahrider@users.noreply.github.com>
2023-02-17 16:50:41 -0500

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,30 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let intervals = [];
+
+document.querySelectorAll(".navbar p").forEach(p => {
+  p.onmouseover = event => {
+    let iteration = 0;
+
+    clearInterval(intervals[p]);
+
+    intervals[p] = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(intervals[p]);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+const learnMoreButton = document.getElementById('learn-more');
+
+learnMoreButton.addEventListener('click', () => {
+  alert('Thanks for clicking! Unfortunately, this button doesn\'t do anything yet. This is just a demo website.');
+});
+
