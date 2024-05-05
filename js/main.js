0413ec18643ce440f2044f5427604d6511004472
Noah Rider <media@noahrider.com>
2024-05-05 01:42:46 -0400

    no more light mode ;)

--- js/main.js
+++ js/main.js
@@ -1,46 +1,22 @@
-// i forgor what this is for honestly
+// cool scroll effect
 document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('a[href^="#"]');
-    
-    for (const link of links) {
-      link.addEventListener("click", clickHandler);
-    }
-    
-    function clickHandler(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      const offsetTop = target.offsetTop;
+  const links = document.querySelectorAll('a[href^="#"]');
   
-      window.scrollTo({
-        top: offsetTop,
-        behavior: "smooth"
-      });
-    }
-  });
+  for (const link of links) {
+    link.addEventListener("click", clickHandler);
+  }
+  
+  function clickHandler(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    const offsetTop = target.offsetTop;
 
-//  toggle between light and dark mode
-function toggleTheme() {
-    const body = document.body;
-    const lightTheme = document.getElementById('light-theme');
-    const darkTheme = document.getElementById('dark-theme');
-    const themeIcon = document.getElementById('theme-icon');
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
+});
 
-    body.classList.toggle('dark-mode');
-    lightTheme.toggleAttribute('disabled');
-    darkTheme.toggleAttribute('disabled');
-
-    // Toggle the theme icon between sun and moon
-    themeIcon.querySelector('i').classList.toggle('fa-sun');
-    themeIcon.querySelector('i').classList.toggle('fa-moon');
-
-    // Toggle dark mode for specific elements if needed
-    const aboutSection = document.getElementById("about-section");
-    aboutSection.classList.toggle("dark-mode");
-
-    // Set the theme in the cookie
-    const theme = body.classList.contains('dark-mode') ? 'dark' : 'light';
-    document.cookie = `theme=${theme};expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-}
-
-// funny console text
-console.log("what are you doing in here? there's not much...")
+// Funny console text
+console.log("Greetings, traveller! I see you like my code ;)")
